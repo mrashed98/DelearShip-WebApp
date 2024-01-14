10fe980c532d6ceb9c49293fae010f3db88aff4b
Mahmoud Rashed <108725449+mrashed98@users.noreply.github.com>
2024-01-14 17:55:59 +0200

    initialCommit

---
+++ .angular/cache/17.0.10/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-HTGB4RKD.js";
+import "./chunk-ORNRHPXP.js";
+import "./chunk-7OYIJYZK.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-TKNFTCHN.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
